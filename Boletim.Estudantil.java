94c1edeb089d384599a63bcb8632525f2035d87e
Natália Farias <nataliafsoficial@gmail.com>
2022-12-15 00:18:43 -0300

    adicionando.código

---
+++ Boletim.Estudantil.java
@@ -0,0 +1,17 @@
+public class BoletimEstudantil{
+   
+    /**
+     * @param aStrings
+     */
+    public static void main(String[]aStrings) {
+        int mediaFinal = 5;
+        if (mediaFinal <6)
+            System.out.println("Reprovado");
+        else if (mediaFinal ==6)
+            System.out.println("Prova Minerva");
+        else
+            System.out.println("Aprovado");
+        
+    }
+
+}
